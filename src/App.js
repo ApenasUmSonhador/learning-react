6df6b601885b11724f3298e0b1bc2b0f8d853680
Arthur Nunes <arthurvininunes@gmail.com>
2026-03-08 18:07:02 -0300

    refactor: replace hardcoded buttons with Square component for tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-export default function Square() {
+import {useState } from 'react';
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  function onSquareClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    const squaresCopy = squares;
+    squaresCopy[i] = xIsNext ? 'X' : 'O';
+    setSquares(squaresCopy);
+    setXIsNext(!xIsNext);
+  }
+
   return <>
   <div className="board-row">
-    <button className="square">1</button> 
-    <button className="square">2</button>
-    <button className="square">3</button>
+    <Square value={squares[0]} onSquareClick={() => onSquareClick(0)}/>
+    <Square value={squares[1]} onSquareClick={() => onSquareClick(1)}/>
+    <Square value={squares[2]} onSquareClick={() => onSquareClick(2)}/>
   </div>    
   <div className="board-row">
-    <button className="square">4</button> 
-    <button className="square">5</button>
-    <button className="square">6</button>
+    <Square value={squares[3]} onSquareClick={() => onSquareClick(3)}/>
+    <Square value={squares[4]} onSquareClick={() => onSquareClick(4)}/>
+    <Square value={squares[5]} onSquareClick={() => onSquareClick(5)}/>
   </div>  
   <div className="board-row">
-    <button className="square">7</button> 
-    <button className="square">8</button>
-    <button className="square">9</button>
+    <Square value={squares[6]} onSquareClick={() => onSquareClick(6)}/>
+    <Square value={squares[7]} onSquareClick={() => onSquareClick(7)}/>
+    <Square value={squares[8]} onSquareClick={() => onSquareClick(8)}/>
   </div>  
   </>;
 }
